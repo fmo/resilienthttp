7e16bb855cb854ca3a0f460116c0cd1d5440804f
Mustafa Ozyurt <mustafa.ozyurt@gmail.com>
2025-10-27 19:41:36 +0100

    wtest for client

---
+++ client_test.go
@@ -0,0 +1,35 @@
+package resilienthttp
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestDo(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("response comes here"))
+	}))
+	defer ts.Close()
+
+	httpReq, _ := http.NewRequest("GET", ts.URL, nil)
+    resilientReq := &Request{Request: httpReq}    
+
+	client := NewClient()
+
+	resp, err := client.Do(resilientReq)
+    if err != nil {
+        t.Errorf("unexpected error %v", err)
+    }
+
+	body, err := io.ReadAll(resp.Body)
+    resp.Body.Close()
+    if err != nil {
+        t.Errorf("unexpected error %v", err)
+    }
+
+	if string(body) != "response comes here" {
+		t.Error("no matching response")
+	}
+}
