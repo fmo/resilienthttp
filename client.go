6154ecafe2c664b4567c7da4e01eb950e9ed34ee
Mustafa Ozyurt <mustafa.ozyurt@gmail.com>
2025-10-27 22:50:00 +0100

    exponential backoff with jitter

--- client.go
+++ client.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 	"log/slog"
+	"math"
+	"math/rand"
 	"net/http"
 	"time"
 )
@@ -12,12 +14,12 @@ import (
 var (
 	defaultRetryMax = 4
 	defaultBackoff  = 1 * time.Second
+	maxBackoff      = 16 * time.Second
 )
 
 type CheckRetry func(res *http.Response) bool
 
-// Client wraps the standard library's http.Client.
-// Adds retry capability for more resilient HTTP requests.
+// Client extens http.Client with configurable retry logic for improved request resilience
 type Client struct {
 	HTTPClient *http.Client
 	RetryMax   int
@@ -30,9 +32,13 @@ type BackoffFunc func(attempt int)
 func NewClient() *Client {
 	return &Client{
 		HTTPClient: &http.Client{},
-		RetryMax:   defaultRetryMax,
+		// Limit the number of retries to give the service time to recover
+		RetryMax: defaultRetryMax,
 		Backoff: func(attempt int) {
-			time.Sleep(defaultBackoff * time.Duration(attempt))
+			backoffTime := time.Duration(math.Min(float64(defaultBackoff)*math.Pow(2, float64(attempt)), float64(maxBackoff)))
+			jitter := time.Duration(rand.Float64() * float64(backoffTime) * 0.5)
+			slog.Info("exponential backoff", "time", backoffTime+jitter)
+			time.Sleep(backoffTime + jitter)
 		},
 		CheckRetry: func(res *http.Response) bool {
 			return res.StatusCode >= 500
@@ -74,7 +80,7 @@ func (c *Client) Do(req *Request) (*http.Response, error) {
 	var response *http.Response
 	var err error
 
-	for i := 1; i <= c.RetryMax; i++ {
+	for i := 0; i < c.RetryMax; i++ {
 		response, err = c.HTTPClient.Do(req.Request)
 		if err != nil {
 			slog.Error("request failed", "err", err)
@@ -87,7 +93,7 @@ func (c *Client) Do(req *Request) (*http.Response, error) {
 
 		if c.CheckRetry(response) {
 			c.Backoff(i)
-			slog.Error("request failed", "attempt", i)
+			slog.Error("request failed", "attempt", i+1)
 			if response != nil {
 				response.Body.Close()
 			}
@@ -95,6 +101,6 @@ func (c *Client) Do(req *Request) (*http.Response, error) {
 		}
 	}
 
-    slog.Error("all retries failed", "retries", c.RetryMax, "last_status", response.StatusCode)
+	slog.Error("all retries failed", "retries", c.RetryMax, "last_status", response.StatusCode)
 	return response, fmt.Errorf("request failed after %d retries: last status=%d", c.RetryMax, response.StatusCode)
 }
